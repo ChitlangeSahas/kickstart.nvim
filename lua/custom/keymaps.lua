114e60eb4a35334802c57079270f04f64ad5dd6c
Chitlange Sahas <chitlangesahas@gmail.com>
2025-12-17 19:49:51 -0700

    feat: remap for rename

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -2,3 +2,9 @@ print 'Loading keymaps.lua'
 
 vim.keymap.set({ 'n', 'v' }, 'x', '"_x', { desc = 'Delete char without yanking' })
 vim.keymap.set({ 'n', 'v' }, 'X', '"_X', { desc = 'Delete char backward without yanking' })
+
+-- U for redo (u is undo, U is redo - makes sense!)
+vim.keymap.set('n', 'U', '<C-r>', { desc = 'Redo' })
+
+-- Then you can use R for rename
+vim.keymap.set('n', 'R', vim.lsp.buf.rename, { desc = 'Rename symbol' })
