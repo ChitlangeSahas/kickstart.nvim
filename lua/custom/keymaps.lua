3d1f62210df82aa1cadbcebbc683be8c1880675a
sahas.chitlange <sahas.chitlange@energyvault.com>
2025-12-25 11:13:16 -0700

    enable arrow keys again

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -4,15 +4,15 @@ vim.keymap.set({ 'n', 'v' }, 'x', '"_x', { desc = 'Delete char without yanking' 
 vim.keymap.set({ 'n', 'v' }, 'X', '"_X', { desc = 'Delete char backward without yanking' })
 
 -- Disabling the arrow keys to get use to the hjkl
-vim.keymap.set({ 'n', 'i', 'v' }, '<Up>', '<Nop>')
-vim.keymap.set({ 'n', 'i', 'v' }, '<Down>', '<Nop>')
-vim.keymap.set({ 'n', 'i', 'v' }, '<Left>', '<Nop>')
-vim.keymap.set({ 'n', 'i', 'v' }, '<Right>', '<Nop>')
+-- vim.keymap.set({ 'n', 'i', 'v' }, '<Up>', '<Nop>')
+-- vim.keymap.set({ 'n', 'i', 'v' }, '<Down>', '<Nop>')
+-- vim.keymap.set({ 'n', 'i', 'v' }, '<Left>', '<Nop>')
+-- vim.keymap.set({ 'n', 'i', 'v' }, '<Right>', '<Nop>')
 -- U for redo (u is undo, U is redo - makes sense!)
 vim.keymap.set('n', 'U', '<C-r>', { desc = 'Redo' })
 
 vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Scroll down and center' })
-vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Scroll up and center' })
+vim.keymap.set('n', '<C-s>', '<C-u>zz', { desc = 'Scroll up and center' })
 
 -- Then you can use R for rename
 vim.keymap.set('n', 'R', vim.lsp.buf.rename, { desc = 'Rename symbol' })
